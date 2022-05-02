931af08801b2b2d88c5144c0aeb83e1110a434cd
pamela <abreupamm@gmail.com>
2022-05-02 18:27:59 -0300

    exercicio de fixação

---
+++ bloco-10-introdução-react/dia-1/execicio-dia-1-fixacao/src/component.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class Hello extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Pamela Silva de Abreu</h1>
+        <p>Sou estudante na trybe</p>
+      </div>
+    );
+  }
+}
+
+export default Hello;
